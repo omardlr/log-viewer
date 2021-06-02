f33d606eb30da13bd876f927803c16e860d31c46
Jose Maldonado <adrianenelpozo@gmail.com>
2021-06-02 06:16:25 -0400

    Recuperacion de cambios perdidos

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,13 +3,17 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Dashboard from '../Pages/Dashboard';
+import Settings from '../Pages/settings/index';
+import Detail from '../Pages/settings/details';
 
 const Routes = () => {
 	return (
 		<Switch>
 			{/* <Route path="/" exact component={SignIn} /> */}
-			<Route path="/" component={Dashboard} isPrivate />
+			<Route path="/" exact component={Dashboard} isPrivate />
 			<Route path="/user" component={Dashboard} isPrivate />
+			<Route path="/Settings" exact component={Settings} isPrivate />
+			<Route path="/Settings/Edit/:id" exact component={Detail} isPrivate />
 
 			{/* redirect user to SignIn page if route does not exist and user is not authenticated */}
 			{/*<Route component={SignIn} />*/}
